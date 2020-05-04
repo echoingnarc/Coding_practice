07bb7df2371dda664d4ad45020cd0f09731d5af8
Chynna <chynna.dehoyos@gmail.com>
2020-05-04 16:28:51 -0500

    Add underscore to demo file

--- learn_js/demo.js
+++ learn_js/demo.js
@@ -1,13 +1,12 @@
 'use strict';
+const _ = require('underscore')
+
 const sentences = [
     { subject: 'Javascript', verb: 'is', object: 'great'},
     { subject: 'Elephants', verb:'are', object:'large'},
 ];
 
-function say({ subject, verb, object }){
-    console.log(`${subject} ${verb} ${object}`);
-}
-
-for(let s of sentences) {
-    say(s);
+const verb = _.pluck(sentences, 'verb')
+for(let verb of verbs){
+    console.log(`found verb: ${verb}`)
 }
